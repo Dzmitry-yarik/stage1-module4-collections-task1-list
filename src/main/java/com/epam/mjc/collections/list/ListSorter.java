7a2d928fa4fd66ecb41441b9e11305a9c70966e6
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-22 20:12:58 +0300

    Update ListSorter.java

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,26 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        
+        sourceList.sort(new ListComparator());
+        Collections.reverse(sourceList);
+        
+        System.out.println(sourceList);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int intA = Integer.parseInt(a);
+        int intB = Integer.parseInt(b);
+        int calculatedA = calc(intA);
+        int calculatedB = calc(intB);
+        return Integer.compare(calculatedB, calculatedA);
+    }
+
+    private int calc(int x) {
+        double form = Math.pow(5*x, 2) + 3;
+        return (int) form;
     }
 }
